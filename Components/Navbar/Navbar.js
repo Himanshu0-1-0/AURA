09005a4891ac5a318294717061ab4b390d106243
Himanshu <himanshumehta8104@gmail.com>
2024-04-09 01:54:06 +0530

    Temporary Navbar Yet Incomplete (transition left)

---
+++ Components/Navbar/Navbar.js
@@ -0,0 +1,30 @@
+'use client'
+import NavbarElement from "./NavbarElement" 
+import "./Navbar.css"
+import { useState,useEffect } from "react"
+export default function Navbar() {
+  const [showNavbar, setShowNavbar] = useState(false);
+
+  useEffect(() => {
+    function handleScroll() {
+      if (window.scrollY > 0) {      
+        setShowNavbar(true);
+      } else {
+        setShowNavbar(false);
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+  
+  return (
+    <>
+    <div className="background">
+      {(showNavbar===false)?<div className="text" id="text">Aura</div>: <NavbarElement />}
+    </div>
+    </>
+  )
+}
