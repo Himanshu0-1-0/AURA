fdcdb37771fc894a214cf2154a14683de6513608
Himanshu <himanshumehta8104@gmail.com>
2024-04-11 03:02:07 +0530

    Button of Card1 Added

--- Components/Cards1/Card.js
+++ Components/Cards1/Card.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import "./Card.css"
-export default function Card({imageid}) {
+export default function Card({imageid,btncontent}) {
   return (
     <>
+    <div className="cont">
     <div className="card">
       <img src={imageid} alt='image ni milri'></img>
+    </div>
+    <button className='butn'>{btncontent}</button>
     </div>
     </>
   )
--- Components/Cards1/MainDiv.js
+++ Components/Cards1/MainDiv.js
@@ -2,9 +2,9 @@ import "./MainDiv.css"
 import Card from "./Card.js"
 export default function MainDiv() {
   return (<div className="card-container">
-     <Card imageid="/Card1-Images/fashion/test.jpg"/>
-      <Card imageid="/Card1-Images/footwear/test.jpg"/>
-      <Card imageid="/Card1-Images/accessories/test.jpg"/>
+     <Card imageid="/Card1-Images/fashion/test.jpg" btncontent="FASHION"/>
+      <Card imageid="/Card1-Images/footwear/test.jpg" btncontent="FOOTWEAR"/>
+      <Card imageid="/Card1-Images/accessories/test.jpg" btncontent="ACCESSORIES"/>
   </div>
   )
 }
