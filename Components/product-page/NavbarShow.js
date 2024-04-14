cace0c7ddcd0876161f9cb71502f6f14079150d3
unknown <poojagusain101@gmail.com>
2024-04-15 01:56:02 +0530

    added incomplete product-page

---
+++ Components/product-page/NavbarShow.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import "./NavbarShow.css"
+
+export default function NavbarShow() {
+    return (
+      <>
+       <nav className= " navbar navbar-expand-lg  fixed-top showNav" id="nav">
+        <div className="container-fluid">
+        <a className="nav-link text-nav " href="#" id="home">Home</a>
+        <div className='logo-aura'>
+                AURA
+        </div>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0 ">
+            <li className="nav-item">
+                <a className="nav-link text-nav " aria-current="page" href="#">Profile</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link text-nav " href="#">About Us</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link text-nav " href="#">Support</a>
+            </li>
+            </ul>
+        </div>
+        </div>
+    </nav>
+      </>
+    )
+}
---
+++ app/[product-page]/page.js
@@ -0,0 +1,15 @@
+import NavbarShow from "@/Components/product-page/NavbarShow.js";
+import ProductPage from "@/Components/product-page/ProductPage.js";
+import Footer from "@/Components/Footer/Footer";
+
+
+
+export default function Product() {
+    return (
+      <>
+        <NavbarShow />
+        <ProductPage/>
+        <Footer/>
+      </>
+    );
+  }
