ec47aa1bf8b28a3731db0e141ef475ac63b9f44c
“Gungun <gugnun3025@yahoo.com>
2024-04-21 16:20:39 +0530

    contact form email integrated

--- Components/Contactform/Contactform.js
+++ Components/Contactform/Contactform.js
@@ -1,18 +1,38 @@
-import React from 'react'
+"use client"
 import './Contactform.css'
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
-export default function Contactform(){
+export default function Contactform() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_1t2xzug', 'template_qxjp84e', form.current, {
+        publicKey: 'QuUCgUcqp5sKKkVOF',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+  };
+
   return (
+
     <div className='form-container'>
-    <h1>Send a msg to us!</h1>
-    <form action="">
-      <input placeholder='Name' type="text" />
-      <input placeholder='Email' type="Email" />
-      <input placeholder='Subject' type="text" />
-      <textarea name="" id="" cols="30" rows="4" placeholder='Message'></textarea>
-      <button>Send Message</button>
-    </form>
-  </div>
+      <h1>Send a msg to us!</h1>
+      <form ref={form} onSubmit={sendEmail}>
+        <input type="text" name="user_name" placeholder='Name'/>
+        <input type="email" name="user_email" placeholder='Email'  />
+        <textarea name="message" cols="30" rows="4" placeholder='Message' />
+        <button type="submit" value="Send"> Submit </button>
+      </form>
+    </div>
   );
 }
-
