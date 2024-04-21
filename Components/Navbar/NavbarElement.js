a84afa11a062db24a7c1bdab5ddf9df43cb3c642
Himanshu <himanshumehta8104@gmail.com>
2024-04-22 00:05:44 +0530

    Dynamic Changes in Navbar Element

--- Components/Navbar/NavbarElement.js
+++ Components/Navbar/NavbarElement.js
@@ -12,9 +12,20 @@ export default function NavbarElement({Show}) {
         <a className={Show ?"nav-link text-nav " :"nav-link text-nav MakeBlue Hide" } href="#" id="home">Home</a>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0 ">
-            <li className="nav-item">
+            <li className="nav-item space">
                 {isLoggedIn?
-                <button className={Show ?"nav-link text-nav " :"nav-link text-nav MakeBlue" } onClick={handleLogout}>Logout</button>
+                // <button className={Show ?"nav-link text-nav " :"nav-link text-nav MakeBlue" } onClick={handleLogout}>Logout</button>
+                    <div class="dropdown">
+                        <button class="btn btn-secondary dropdown-toggle prof" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+                                <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z"/>
+                            </svg>
+                        </button>
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                            <a class="dropdown-item" href="#">Profile</a>
+                            <button class="dropdown-item" onClick={handleLogout}>Logout</button>
+                        </div>
+                    </div>
                 :<Link className={Show ?"nav-link text-nav " :"nav-link text-nav MakeBlue" } aria-current="page" href="/login?state=login">Login</Link>}
             </li>
             <li className="nav-item">
--- app/layout.js
+++ app/layout.js
@@ -15,7 +15,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Bootstrapjs/>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>{children}
+      <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+          <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+          <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+      </body>
      </html>
   );
 }
